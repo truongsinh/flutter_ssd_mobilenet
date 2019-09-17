7e64f597f607e830f4c8f41c2bec611b86262ecd
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2019-09-17 23:13:42 +0700

    working, tested on pixel 2 and iPhone 6

---
+++ lib/image_processing.dart
@@ -0,0 +1,75 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:image/image.dart' as imglib;
+
+imglib.Image preprocessImage(CameraImage camImg) {
+  final imgData640x480 = convertCameraImageToImageColor(camImg);
+  final imgData = imglib
+      .copyRotate(
+        imglib.copyResizeCropSquare(imgData640x480, 300),
+        // camera plugin on Android sucks
+        Platform.isAndroid ? 90 : 0,
+      );
+  return imgData;
+}
+
+imglib.Image convertCameraImageToImageColor(CameraImage image) {
+  if (image.format.group == ImageFormatGroup.bgra8888) {
+    return convertBGRA8888toImageColor(image);
+  } else if (image.format.group == ImageFormatGroup.yuv420) {
+    return convertYUV420toImageColor(image);
+  } else {
+    throw Exception("unkown format group");
+  }
+}
+
+imglib.Image convertBGRA8888toImageColor(CameraImage image) {
+  final result = imglib.Image.fromBytes(
+    image.width,
+    image.height,
+    image.planes[0].bytes,
+    format: imglib.Format.bgra,
+  );
+  return result;
+}
+
+imglib.Image convertYUV420toImageColor(CameraImage image) {
+  try {
+    final int width = image.width;
+    final int height = image.height;
+    final int uvRowStride = image.planes[1].bytesPerRow;
+    final int uvPixelStride = image.planes[1].bytesPerPixel;
+
+    const alpha255 = (0xFF << 24);
+
+    // imgLib -> Image package from https://pub.dartlang.org/packages/image
+    final img = imglib.Image(width, height); // Create Image buffer
+
+    // Fill image buffer with plane[0] from YUV420_888
+    for (int x = 0; x < width; x++) {
+      for (int y = 0; y < height; y++) {
+        final int uvIndex =
+            uvPixelStride * (x / 2).floor() + uvRowStride * (y / 2).floor();
+        final int index = y * width + x;
+
+        final yp = image.planes[0].bytes[index];
+        final up = image.planes[1].bytes[uvIndex];
+        final vp = image.planes[2].bytes[uvIndex];
+        // Calculate pixel color
+        int r = (yp + vp * 1436 / 1024 - 179).round().clamp(0, 255);
+        int g = (yp - up * 46549 / 131072 + 44 - vp * 93604 / 131072 + 91)
+            .round()
+            .clamp(0, 255);
+        int b = (yp + up * 1814 / 1024 - 227).round().clamp(0, 255);
+        // color: 0x FF  FF  FF  FF
+        //           A   B   G   R
+        img.data[index] = alpha255 | (b << 16) | (g << 8) | r;
+      }
+    }
+    return img;
+  } catch (e) {
+    print(">>>>>>>>>>>> ERROR:" + e.toString());
+  }
+  return null;
+}
